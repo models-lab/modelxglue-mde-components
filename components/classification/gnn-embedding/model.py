70835a5b5b533de1e20ebf09d61d7d9301a2f5c4
Jesús Sánchez Cuadrado <jesusc@um.es>
2024-04-25 18:24:35 +0200

    GNN with WordE4MDE embeddings

---
+++ components/classification/gnn-embedding/model.py
@@ -0,0 +1,38 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(__file__))
+from utils.model_io import ModelInOut, execute_model, ModelImplementation
+from GNN import GNNClassifier
+
+
+def set_seed(seed: int):
+    import random
+    import numpy as np
+    import torch
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+class Model(ModelImplementation):
+    def train(self, X, y, inout: ModelInOut):
+        set_seed(inout.seed)
+
+        model = GNNClassifier(**inout.hyper)
+        model.fit(X['graph'], y)
+        return model
+
+    def test(self, loaded_model, X, inout: ModelInOut):
+        y_pred = loaded_model.predict(X['graph'])
+        return y_pred
+
+
+
+def main(inout: ModelInOut):
+    inout.execute_model(Model())
+
+
+if __name__ == "__main__":
+    execute_model(main)
+    exit(0)
