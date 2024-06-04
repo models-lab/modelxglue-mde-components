be871c2e14023a925c5a3f649ef42e9d26a6710b
Carlozzito <carlozzito@hotmail.com>
2024-06-04 10:01:36 +0000

    Now ModelXGlue works with fasttext-mde and sgram-mde embeddings

--- components/recommendation/ecorebert/download_model.sh
+++ components/recommendation/ecorebert/download_model.sh
@@ -1,6 +1,6 @@
 wget https://zenodo.org/record/5579980/files/models.7z
 wget https://zenodo.org/record/5579980/files/tokenizers.7z
-7z x models.7z
-7z x tokenizers.7z
+7zz x models.7z
+7zz x tokenizers.7z
 rm models.7z
 rm tokenizers.7z
